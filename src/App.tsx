d4b743b5eb691b826e6ac0fb23b26b2474b5fbf5
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-10-31 15:29:00 +0000

    feat: add user search component with GitHub API fetch using TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import UserSearch from "./assets/components/UserSearch";
+
 const App = () => {
   return (
-    <section className="grid min-h-screen place-items-center">My App</section>
+    <section className="my-[130px] mx-[355px]">
+      <div className="flex justify-between items-center">
+        <h1 className="text-preset-1 text-neutral-900">devfinder</h1>
+        <div className="flex items-center gap-4">
+          <p className="text-preset-8 text-neutral-500">DARK</p>
+          <img src="/images/icon-moon.svg" alt="Moon icon for dark mode" />
+        </div>
+      </div>
+      <UserSearch />
+    </section>
   );
 };
 
---
+++ src/assets/components/UserSearch.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const UserSearch = () => {
+  const [username, setUsername] = useState("");
+  const [submittedUsername, setSubmittedUsername] = useState("");
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["users", submittedUsername],
+    queryFn: async () => {
+      const res = await fetch(
+        `${import.meta.env.VITE_GITHUB_API_URL}/users/${submittedUsername}`
+      );
+      if (!res.ok) throw new Error("No results");
+
+      const data = await res.json();
+      // console.log(data);
+      return data;
+    },
+    enabled: !!submittedUsername,
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmittedUsername(username.trim());
+  };
+
+  return (
+    <section className="my-10">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center gap-5 bg-neutral-0 py-2 pl-6 pr-3 rounded-2xl shadow-dropdown"
+      >
+        <img src="/images/icon-search.svg" alt="Search icon" />
+        <input
+          type="text"
+          placeholder="Search GitHub username…"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="w-full text-preset-3 text-neutral-700 placeholder-neutral-500 outline-none focus:outline-none"
+        />
+        <button className="btn" type="submit">
+          Search
+        </button>
+      </form>
+
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>{error.message}</p>}
+
+      {data && (
+        <div>
+          <img
+            src={data.avatar_url}
+            alt={data.name}
+            className="rounded-full h-[117px]"
+          />
+          <h2>{data.name || data.login}</h2>
+          <p>{`Joined ${data.created_at}`}</p>
+          <p>{data.login}</p>
+          <p>{data.bio}</p>
+          <p>{`Repos ${data.public_repos}`}</p>
+          <p>{`Followers ${data.followers}`}</p>
+          <p>{`Following ${data.following}`}</p>
+          <div className="flex">
+            <img src="/images/icon-location.svg" alt={data.location} />
+            <p>{data.location || "Not Available"}</p>
+          </div>
+          <div className="flex">
+            <img src="/images/icon-website.svg" alt={data.blog} />
+            <p>{data.blog || "Not Available"}</p>
+          </div>
+          <div className="flex">
+            <img src="/images/icon-twitter.svg" alt={data.twitter_username} />
+            <p>{data.twitter_username || "Not Available"}</p>
+          </div>
+          <div className="flex">
+            <img src="/images/icon-company.svg" alt={data.company} />
+            <p>{data.company || "Not Available"}</p>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default UserSearch;
